452c2c8e51bc58c03daab511df2daa82183b245e
ZHANG Qichuan <qichuan@zhangqichuan.com>
2019-07-06 15:45:33 +0100

    Reuse fragment_button_and_text

--- wear/src/main/java/com/klinker/android/twitter_l/fragment/RetweetButtonFragment.java
+++ wear/src/main/java/com/klinker/android/twitter_l/fragment/RetweetButtonFragment.java
@@ -1,19 +1,16 @@
 package com.klinker.android.twitter_l.fragment;
 
 import android.app.Fragment;
-import android.content.SharedPreferences;
 import android.os.Bundle;
-import android.preference.PreferenceManager;
-import android.support.wearable.view.CircledImageView;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
-import android.widget.Toast;
+import android.widget.ImageView;
+import android.widget.TextView;
 
 import com.klinker.android.twitter_l.R;
-import com.klinker.android.twitter_l.activity.SettingsActivity;
 import com.klinker.android.twitter_l.activity.WearActivity;
-import com.klinker.android.twitter_l.transaction.KeyProperties;
+import com.klinker.android.twitter_l.view.MyFrameLayout;
 
 public class RetweetButtonFragment extends Fragment {
 
@@ -31,8 +28,14 @@ public class RetweetButtonFragment extends Fragment {
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup parent, Bundle savedInstanceState) {
-        View view = inflater.inflate(R.layout.fragment_retweet_button, parent, false);
-        CircledImageView button = (CircledImageView) view.findViewById(R.id.retweet_button);
+        View view = inflater.inflate(R.layout.fragment_button_and_text, parent, false);
+        MyFrameLayout button = view.findViewById(R.id.button);
+        ImageView imageView = view.findViewById(R.id.image);
+        TextView textView = view.findViewById(R.id.text);
+
+        button.updateBackgroundDrawableColor();
+        imageView.setImageResource(R.drawable.ic_wear_retweet);
+        textView.setText(R.string.retweet);
         button.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
@@ -40,9 +43,6 @@ public class RetweetButtonFragment extends Fragment {
             }
         });
 
-        SharedPreferences sharedPreferences = PreferenceManager.getDefaultSharedPreferences(getActivity());
-        int accentColor = sharedPreferences.getInt(KeyProperties.KEY_ACCENT_COLOR, getResources().getColor(R.color.orange_accent_color));
-        button.setCircleColor(accentColor);
         return view;
     }
 
